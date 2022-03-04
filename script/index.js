c27c107284fc8a3c33e7525646debe68811511a8
Rohan <rohansonawane1818@gmail.com>
2022-03-04 15:12:08 +0530

    added carousel

--- script/index.js
+++ script/index.js
@@ -1,5 +1,5 @@
-$(document).ready(function(){
-    $(".toggle-nav").click(function(){
-        $(".menu ul li").slideToggle("slow");
-    });
-});
+// $(document).ready(function(){
+//     $(".toggle-nav").click(function(){
+//         $(".menu ul li").slideToggle("slow");
+//     });
+// });
